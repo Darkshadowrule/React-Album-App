f53696bd559396cb156938fa6be63c6c1f71ca95
darkshadowrule <prajjwaldxi3@gmail.com>
2019-03-04 22:20:13 +0530

    Added Image of Album

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,21 @@ import Card from './card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image,image } = album;
 return (
 <Card> 
     <CardSection>
-        <View>
-            <Image style = {styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+        <View style={styles.thumbnailContainerStyle}>
+            <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={styles.headerContentStyle}>
-        <Text>{title}</Text>
+        <Text style = {styles.headerTextStyle}>{title}</Text>
         <Text>{artist}</Text>
         </View>
     
+    </CardSection>
+    <CardSection>
+        <Image source={{ uri: image }} style= {styles.imageStyle}/>
     </CardSection>
 </Card>
 
@@ -27,9 +30,24 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around',
     },
+    headerTextStyle:{
+  fontSize: 18,
+    },
+
     thumbnailStyle: {
          height: 50,
          width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle:{
+        height:300,
+        flex: 1,
+        width:null
     }
 }
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -18,9 +18,9 @@ class AlbumList extends React.Component {
         console.log(this.state);
         console.log('====================================');
     return (
-        <View>
+        <ScrollView>
             {this.renderAlbums()}
-        </View>
+        </ScrollView>
     );
 }
      
